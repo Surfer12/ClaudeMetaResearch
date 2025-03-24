65f7b6a5091ef529b324f0ff9174c2005d441d2a
surfer12 <ryanoatsie@outlook.com>
2025-03-23 21:43:17 -0700

    Add wrangler config and update dependencies

--- hello-ai/src/index.ts
+++ hello-ai/src/index.ts
@@ -8,9 +8,30 @@ import { TaskList } from "./endpoints/taskList";
 // Start a Hono app
 const app = new Hono();
 
+async function run(model: string, prompt: string) {
+  const messages = [{ role: "user", content: prompt }];
+
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/${model}`,
+    {
+      headers: { Authorization: "Bearer {API_TOKEN}" },
+      method: "POST",
+      body: JSON.stringify({ messages }),
+    },
+  );
+  const result = await response.json();
+  return result;
+}
+
+run("@cf/mistral/mistral-7b-instruct-v0.1", "[INST] 2 + 2 ? [/INST]").then(
+  (response) => {
+    console.log(JSON.stringify(response));
+  },
+);
+
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
-	docs_url: "/",
+  docs_url: "/",
 });
 
 // Register OpenAPI endpoints
